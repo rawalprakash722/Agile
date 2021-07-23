a7fd3f6fb117deae98b4495e66375ddcdca63ea5
rawalprakash722 <rawalprakash722@gmail.com>
2021-07-23 22:21:16 +0545

    resturant model

--- models/resturant.js
+++ models/resturant.js
@@ -13,4 +13,31 @@ const resturantSchema = new mongoose.Schema({
     }
 },{timestamps:true});
 
+
+const mongoose = require('mongoose');
+const foodSchema = new mongoose.Schema({
+    foodname:{
+        type:String,
+     
+    },
+    foodimage:{
+        type:String
+    },
+    price:{
+        type:String,
+     
+    },
+    restaurant:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref:"Resturant"
+    },
+    category:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref:"FoodCategory"
+    },
+},{timestamps:true});
+
+module.exports=mongoose.model('Food',foodSchema);
+
 module.exports = mongoose.model('Resturant',resturantSchema);
+

fc3d3aae083b46d1c7a983d534bc524a3aa1bde5
rawalprakash722 <rawalprakash722@gmail.com>
2021-08-10 20:41:52 +0545

    view order route

---
+++ routes/vieworder.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const Order = require('../models/order');
+const router = express.Router();
+
+
+var populateQuery = [{path:'user'}, {path:'food'}];
+// router.get('/orders/show/admin',function(req,res){
+//     Order.find().then(function(data){
+//         res.send(data);
+//     })
+// })
+router.route('/orders/show/admin')
+.get((req,res,next)=>{
+    Order.find()
+    .populate(populateQuery)
+    .then((order)=>{
+        res.statusCode = 200;
+        res.json(order);
+    })
+})
+
+
+
+var populateQuery = [{path:'user'}, {path:'food'}];
+// router.get('/orders/show/admin',function(req,res){
+//     Order.find().then(function(data){
+//         res.send(data);
+//     })
+// })
+router.route('/orders/show/admin')
+.get((req,res,next)=>{
+    Order.find()
+    .populate(populateQuery)
+    .then((order)=>{
+        res.statusCode = 200;
+        res.json(order);
+    })
+})
+
+
+module.exports = router;
